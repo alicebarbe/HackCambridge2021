5f43f3d74688975ebcea7bbcf6bc744b2e0febbd
Kristina Pavlou <2431835p@student.gla.ac.uk>
2021-01-24 00:19:16 +0200

    almost done with homepage

--- Quest/src/app/app.module.ts
+++ Quest/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,8 +13,11 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Quest/src/app/home/home.component.ts
+++ Quest/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    const minRadius = 0.5;
+    const maxRadius = 20;
+    this.form = this.fb.group({
+      location: ['', [
+        Validators.required,
+        Validators.pattern('^(([A-Z][0-9]{1,2})|(([A-Z][A-HJ-Y][0-9]{1,2})|(([A-Z][0-9][A-Z])|([A-Z][A-HJ-Y][0-9]?[A-Z])))) [0-9][A-Z]{2}$')
+      ]],
+      radius: ['', [
+        Validators.required,
+        Validators.min(minRadius),
+        Validators.max(maxRadius)
+      ]]
+    })
+  }
+
+  public submit(f: FormGroup) {
+    console.log(this.form.get('location'));
+    console.log(this.form.get('radius'));
+  }
 
   ngOnInit(): void {
   }
